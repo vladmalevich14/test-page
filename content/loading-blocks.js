37b35d6301609fa5ee378a9ca4a2c7e807a18760
vladmalevich14 <vladmalevich1431@gmail.com>
2024-03-17 15:41:16 +0300

    bug fixing

--- content/loading-blocks.js
+++ content/loading-blocks.js
@@ -1,12 +1,11 @@
 fetch('content/header/header.html')
     .then(response => response.text())
-    .then(html => document.getElementById('header').insertAdjacentHTML('beforeend', html));
+    .then(html => document.getElementById('header').innerHTML = html);
 
-fetch('content/main/__description/main__description.html')
+fetch('content/main/main__description.html')
     .then(response => response.text())
-    .then(html => document.getElementById('about').insertAdjacentHTML('beforeend', html));
+    .then(html => document.getElementById('about').innerHTML = html);
 
 fetch('content/footer/footer.html')
     .then(response => response.text())
-    .then(html => document.getElementById('footer').insertAdjacentHTML('beforeend', html));
-
+    .then(html => document.getElementById('footer').innerHTML = html);
